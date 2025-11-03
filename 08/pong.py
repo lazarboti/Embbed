7a237930a7c493b7cb16ad82ea25b5f202ebd516
lazarboti <161580615+lazarboti@users.noreply.github.com>
2025-11-03 13:04:29 +0100

    v2
    Refactor pong game logic to improve readability and functionality.

--- 08/pong.py
+++ 08/pong.py
@@ -1,61 +1,87 @@
 from sense_hat import SenseHat
-from time import sleep
 import random
+from time import sleep
 
 sense = SenseHat()
-sense.clear()
-
-bat_y = 4
+speed = 0.3
+bat = [7, 3]
 score = 0
+up_down = 1
+
+w = (0,0,0)
+r = (255,0,0)
+b = (0,0,255)
+
+game_space = [w]*64
+game_space[7*8 + bat[1]] = b
+game_space[7*8 + bat[1]+1] = b
+
+def update_space(x, y, colour):
+    if 0 <= x < 8 and 0 <= y < 8:
+        game_space[x*8 + y] = colour
+        sense.set_pixels(game_space)
+
+def draw_bat():
+    update_space(7, bat[1], b)
+    update_space(7, bat[1]+1, b)
+
+def left(event):
+    if event.action == 'pressed':
+        if bat[1] > 0:
+            update_space(7, bat[1]+1, w)
+            bat[1] -= 1
+            draw_bat()
+
+def right(event):
+    if event.action == 'pressed':
+        if bat[1] < 6:
+            update_space(7, bat[1], w)
+            bat[1] += 1
+            draw_bat()
+
+sense.stick.direction_left = left
+sense.stick.direction_right = right
+
+sense.clear()
+draw_bat()
+game_alive = True
+
 x = 0
-y = random.randint(0, 7)
-d = random.choice([-1, 1])
-up_down = -1
-
-r = [[0]*8 for _ in range(8)]
-
-def update_space(x, y, r):
-    sense.clear()
-    for i in range(8):
-        for j in range(8):
-            if r[i][j] == 1:
-                sense.set_pixel(i, j, (0, 255, 0))
-    if 0 <= x <= 7 and 0 <= y <= 7:
-        sense.set_pixel(x, y, (255, 0, 0))
-
-def move_up(event):
-    global bat_y
-    if event.action == 'pressed' and bat_y > 0:
-        bat_y -= 1
-
-def move_down(event):
-    global bat_y
-    if event.action == 'pressed' and bat_y < 7:
-        bat_y += 1
-
-sense.stick.direction_up = move_up
-sense.stick.direction_down = move_down
-
-r[7][bat_y] = 1
+y = random.randint(0,7)
+d = random.choice([-1,1])
 update_space(x, y, r)
 
-while True:
-    sleep(0.3)
-    sense.clear()
-    r = [[0]*8 for _ in range(8)]
-    r[7][bat_y] = 1
-    x += d
-    y += up_down
+while game_alive:
+    sleep(speed)
+    if 0 <= x < 8 and 0 <= y < 8:
+        update_space(x, y, w)
 
-    if y <= 0 or y >= 7:
-        up_down *= -1
-    if x == 7 and y == bat_y:
-        d *= -1
-        score += 1
-    if x > 7 or x < 0:
-        break
+    x += up_down
+    y += d
+
+    if y >= 7:
+        y = 7
+        d = -1
+    elif y <= 0:
+        y = 0
+        d = 1
+
+    if x >= 7:
+        x = 7
+        if y == bat[1] or y == bat[1]+1:
+            score += 1
+            up_down = -1
+        else:
+            game_alive = False
+            break
+    elif x <= 0:
+        x = 0
+        up_down = 1
 
     update_space(x, y, r)
+    draw_bat()
 
-sense.show_message("Game over! Score: {}".format(score), text_colour=(255, 255, 255))
 sense.clear()
+sense.show_message('Game over!', scroll_speed=0.05, back_colour=w)
+sense.show_message('Score: ' + str(score), scroll_speed=0.01, back_colour=w)
+
